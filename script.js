a8240b697b376244f63d0bbe68f2d5223adc85fb
MoodyShop <130382557+asliddinesport@users.noreply.github.com>
2023-10-27 19:07:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+/ Код для обработки формы входа в аккаунт
+var loginForm = document.getElementById("login-form");
+
+if (loginForm) {
+  loginForm.addEventListener("submit", function(e) {
+    e.preventDefault(); // Отменяем стандартное поведение отправки формы
+
+    // Получаем значения полей формы
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Здесь можно добавить логику проверки введенных данных
+
+    alert("Вы вошли в аккаунт!"); // Вместо этого можно добавить редирект на страницу аккаунта
+  });
+}
 let btn = document.querySelector("#btn");
 let sidebar = document.querySelector(".sidebar");
 let searchBtn = document.querySelector(".bx-search");
